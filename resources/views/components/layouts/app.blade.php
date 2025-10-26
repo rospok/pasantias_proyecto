9a3db2638605fb473dc111ba54d6f027f670495f
Rospok <gabriel252318@gmail.com>
2025-10-26 16:54:22 -0400

    mejoras diseño

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <title>{{ $title ?? 'Page Title' }}</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
         {{-- <link href="{{ asset('tailadmin/css/style.css') }}" rel="stylesheet"> --}}
         @vite(['resources/css/style.css'])
+        @livewireStyles
+        {{-- <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" /> --}}
+
     </head>
     
   <body
@@ -15,6 +21,7 @@
          darkMode = JSON.parse(localStorage.getItem('darkMode'));
          $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
     :class="{'dark bg-gray-900': darkMode === true}"
+    :data-theme="darkMode ? 'dark' : 'light'"
   >
 <!-- ===== Preloader Start ===== -->
     <x-preloader />
@@ -59,7 +66,8 @@
       <!-- ===== Content Area End ===== -->
     </div>
     <!-- ===== Page Wrapper End ===== -->
-
+@livewireScripts
+{{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> --}}
     </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ Route::get('/', function () {
 });
 
 Route::get('/prueba', App\Livewire\Prueba::class)->name('prueba');
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
 
 Route::get('/view', function () {
     return view('view');
